4d2d301a001f7477c8328c1c0e3a556179dd9b2f
Макс Титов <hhit934@gmail.com>
2025-03-10 21:11:38 +0300

    Добавьте файлы проекта.

---
+++ Waifu.Application/View/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using WPF.DarkGalaxy.Controls;
+
+namespace Waifu.Application.View;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : DarkWindow
+{
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+
+}
