84107a241b6f99e06bf17b12484757d4fe4ba2a6
Макс Титов <hhit934@gmail.com>
2025-03-10 22:44:15 +0300

    fix

---
+++ Waifu.Application/View/Controls/SplitView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Waifu.Application.View.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для SplitView.xaml
+    /// </summary>
+    public partial class SplitView : UserControl
+    {
+        private bool IsDown = false;
+        public SplitView()
+        {
+            InitializeComponent();
+
+
+            grid.MouseMove += Grid_MouseMove;
+            Loaded += SplitView_Loaded;
+        }
+
+        private void SplitView_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetRecPos(ActualWidth / 2);
+        }
+
+        private void Grid_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+
+            {
+                double x = e.GetPosition(this).X;
+                SetRecPos(x);
+            }
+
+        }
+
+        void SetRecPos(double x)
+        {
+            x = x < 0 ? 0 : x > ActualWidth - 0.1 ? ActualWidth : x;
+            x = ActualWidth - x;
+
+
+            rec0.Rect = new Rect(0, 0, ActualWidth - x, ActualHeight);
+            rec1.Rect = new Rect(ActualWidth - x, 0, ActualWidth, ActualHeight);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Waifu.Application/View/Controls/WScrollBar.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Waifu.Application.View.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для WScrollBar.xaml
+    /// </summary>
+    public partial class WScrollBar : Border
+    {
+        public bool IsPress
+        {
+            get; set;
+        }
+        public double XPos
+        {
+            get; set;
+        }
+        public double XBorderPos
+        {
+            get; set;
+        }
+        double buttonpos = 0;
+
+
+        public double value
+        {
+            get; set;
+        }
+
+        public WScrollBar()
+        {
+            InitializeComponent();
+            button.MouseLeftButtonDown += Border_MouseLeftButtonDown;
+            button.MouseLeftButtonUp += Border_MouseLeftButtonUp;
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            //IsPress = false;
+            base.OnMouseLeave(e);
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            XPos = e.GetPosition(this).X;
+
+
+            if (IsPress)
+            {
+                buttonpos = XPos - XBorderPos;
+                buttonpos = buttonpos < 0 ? 0 : (buttonpos > ActualWidth - button.Width ? ActualWidth - button.Width : buttonpos);
+                button.Margin = new System.Windows.Thickness(buttonpos, 0, 0, 0);
+            }
+            //ActualWidth / 100 +
+            value = (XPos + button.Width / 2);
+
+            Debug.WriteLine(value);
+            base.OnMouseMove(e);
+        }
+
+        private void Border_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            IsPress = false;
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            XBorderPos = Mouse.GetPosition(button).X;
+            IsPress = true;
+        }
+    }
+}
